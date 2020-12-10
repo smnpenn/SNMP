80ced510fbe600ff84623bf1f0acab0e44e9f730
Simse <smnpenn@gmail.com>
2020-12-10 18:51:36 +0100

    SDepp

---
+++ main.py
@@ -0,0 +1,12 @@
+import snmp
+
+if __name__ == "__main__":
+    oidarray = []
+    oidarray.append(".1.3.6.1.2.1.1.1.0")
+    oidarray.append(".1.3.6.1.2.1.1.5.0")
+    oidarray.append(".1.3.6.1.2.1.1.4.0")
+    oidarray.append(".1.3.6.1.2.1.25.2.2.0")
+
+    resultarray = snmp.get("localhost", oidarray, "public")
+
+    print(resultarray[1])
